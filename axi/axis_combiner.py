8174d7c4d9ec7f09227cc40cd7809922542928ef
Ben Reynwar <ben@reynwar.net>
2015-01-23 14:59:48 -0700

    Working on integration of Xilinx IP modules.  Testing with axis_combiner.

---
+++ axi/axis_combiner.py
@@ -0,0 +1,61 @@
+from pyvivado import builder, interface, signal
+
+class AxisCombinerBuilder(builder.Builder):
+    
+    def __init__(self, params):
+        super().__init__(params)
+        n_input_streams = params['n_input_streams']
+        input_stream_width = params['input_stream_width']
+        assert(input_stream_width % 8 == 0)
+        module_name = params['module_name']
+        ip_params = {
+            'HAS_ACLKEN': 0,
+            # Produces error signal if meta-info for input streams does not
+            # match
+            'HAS_CMD_ERR': 0, 
+            'HAS_TKEEP': 0,
+            'HAS_TLAST': 1,
+            'HAS_TSTRB': 0,
+            'MASTER_PORT_NUM': 0,
+            'NUM_SI': n_input_streams,
+            'TDATA_NUM_BYTES': input_stream_width//8,
+            'TDEST_WIDTH': 0,
+            'TID_WIDTH': 0,
+            'TUSER_WIDTH': 0,
+        }
+        self.ips = [
+            ('axis_combiner', ip_params, module_name),
+        ]
+
+        
+def get_axis_combiner_interface(params):
+    factory_name = 'AxisCombiner'
+    module_name = params['module_name']
+    n_input_streams = params['n_input_streams']
+    input_stream_width = params['input_stream_width']
+    builder = AxisCombinerBuilder(params)
+    packages = []
+    module_parameters = {}
+    output_width = n_input_streams * input_stream_width
+    wires_in = (
+        ('aresetn', signal.std_logic_type),
+        ('s_axis_tdata', signal.StdLogicVector(width=output_width)),
+        ('s_axis_tlast', signal.StdLogicVector(width=n_input_streams)),
+        ('s_axis_tvalid', signal.StdLogicVector(width=n_input_streams)),
+        ('m_axis_tready', signal.std_logic_type),
+    )
+    wires_out = (
+        ('m_axis_tdata', signal.StdLogicVector(width=output_width)),
+        ('m_axis_tlast', signal.std_logic_type),
+        ('m_axis_tvalid', signal.std_logic_type),
+        ('s_axis_tready', signal.StdLogicVector(width=n_input_streams)),
+    )
+    iface = interface.Interface(
+        wires_in, wires_out, module_name=module_name, factory_name=factory_name,
+        parameters=params, module_parameters=module_parameters,
+        packages=packages, builder=builder, clock_name='aclk', needs_dummy=True)
+    return iface
+
+
+assert('AxisCombiner' not in interface.module_register)
+interface.module_register['AxisCombiner'] = get_axis_combiner_interface
---
+++ axi/qa_axis_combiner.py
@@ -0,0 +1,105 @@
+import os
+import unittest
+import shutil
+import random
+import logging
+
+import testfixtures
+
+from pyvivado import project, signal
+from pyvivado import config as pyvivado_config
+
+from rfgnocchi.axi import axis_combiner
+from rfgnocchi import config
+
+logger = logging.getLogger(__name__)
+
+class TestAxiPipe(unittest.TestCase):
+    
+    def test_one(self):
+
+        directory = os.path.abspath('proj_qa_testaxiscombiner')
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+        os.mkdir(directory)
+
+        n_input_streams = 3
+        input_stream_width = 24
+
+        params = {
+            'module_name': 'axis_combiner_0',
+            'n_input_streams': n_input_streams,
+            'input_stream_width': input_stream_width,
+        }
+
+        # Make wait data.  Sent while initialising.
+        n_data = 10
+        n_wait_lines = 20
+        wait_data = []
+        for wait_index in range(n_wait_lines):
+            input_d = {
+                'a_resetn': 0,
+                's_axis_tdata': signal.list_of_uints_to_uint(
+                    [0, 0, 0], input_stream_width),
+                's_axis_tlast': signal.list_of_uints_to_uint(
+                    [0, 0, 0], 1),
+                's_axis_tvalid': signal.list_of_uints_to_uint(
+                    [0, 0, 0], 1),
+                'm_axis_tready': 0,
+            }
+            wait_data.append(input_d)
+        # Make input and expected data
+        input_data = []
+        expected_data = []
+        for data_index in range(n_data):
+            input_d = {
+                'a_resetn': 1,
+                's_axis_tdata': signal.list_of_uints_to_uint(
+                    [3, 4, 5], input_stream_width),
+                's_axis_tlast': signal.list_of_uints_to_uint(
+                    [0, 0, 0], 1),
+                's_axis_tvalid': signal.list_of_uints_to_uint(
+                    [1, 1, 1], 1),
+                'm_axis_tready': 1,
+            }
+            expected_d = {
+                'm_axis_tdata': input_d['s_axis_tdata'],
+                'm_axis_tlast': input_d['s_axis_tlast'],
+                'm_axis_tvalid': input_d['s_axis_tvalid'],
+                's_axis_tready': signal.list_of_uints_to_uint(
+                    [1, 1, 1], 1),
+            }
+            input_data.append(input_d)
+            expected_data.append(expected_d)
+
+        interface = axis_combiner.get_axis_combiner_interface(params)
+        p = project.FileTestBenchProject.create(
+            interface=interface, directory=directory,
+            board=config.default_board,
+            part=config.default_part,
+        )
+        t = p.wait_for_most_recent_task()
+        errors = t.get_errors()
+        self.assertEqual(len(errors), 0)
+
+        # Run the simulation
+        runtime = '{} ns'.format((len(input_data) + 20) * 10)
+        errors, output_data = p.run_hdl_simulation(
+            input_data=wait_data+input_data, runtime=runtime)
+        latency = 0
+        delay = 1 + n_wait_lines + latency
+        import pdb
+        pdb.set_trace()
+        self.check_output(output_data[delay:], expected_data)
+        self.assertEqual(len(errors), 0)
+
+    def check_output(self, output_data, expected_data):
+        self.assertTrue(len(output_data) >= len(expected_data))
+        output_data = output_data[:len(expected_data)]
+        testfixtures.compare(output_data, expected_data)
+        
+        
+if __name__ == '__main__':
+    pyvivado_config.use_test_db()
+    config.setup_logging(logging.DEBUG)
+    unittest.main()
