558006ee366f25bbd1c925c3819953d1afb9511d
Ben Reynwar <ben@reynwar.net>
2015-04-30 13:38:48 -0700

    noc_block_fir_filter is not working in the test.

--- ettus/rfnoc/qa_noc_block_fir_filter.py
+++ ettus/rfnoc/qa_noc_block_fir_filter.py
@@ -9,7 +9,7 @@ from pyvivado import project, signal
 from pyvivado import config as pyvivado_config
 
 from rfgnocchi.ettus.rfnoc import noc_block_fir_filter
-from rfgnocchi import config, chdr
+from rfgnocchi import config, chdr, noc
 
 logger = logging.getLogger(__name__)
 
@@ -17,10 +17,8 @@ class TestNocBlockFirFilter(unittest.TestCase):
     
     def test_one(self):
         '''
-        This doesn't test functionality. Just that everything gets built
-        and simulations.
-
-        Currently it is not functional.
+        Tests the noc_block_fir_filter module using random coefficients and
+        data.
         '''
 
         directory = os.path.abspath('proj_qa_testnocblockfirfilter')
@@ -31,7 +29,7 @@ class TestNocBlockFirFilter(unittest.TestCase):
 
         # Make a settings packet to set the taps of the filter.
         reload_address = 129
-        config_address = 130
+        config_address = 131
 
         max_coeff = pow(2, coeff_width-1)-1
         min_coeff = -pow(2, coeff_width-1)
@@ -40,15 +38,17 @@ class TestNocBlockFirFilter(unittest.TestCase):
         uint_coefficients = [signal.sint_to_uint(c, width=coeff_width)
                              for c in coefficients]
         # The reload commands send the taps.
-        settings_commands = [(reload_address, c) for c in uint_coefficients]
-        # And the config command activatees them.
+        settings_commands = [[reload_address, c] for c in uint_coefficients]
+        # Change address in final setting to assert tlast.
+        settings_commands[-1][0] = reload_address+1
+        # And the config command activates them.
         settings_commands.append((config_address, 0))
         settings_packets = [chdr.CHDRPacket.create_settings_packet(
             address=address, value=value)
                             for address, value in settings_commands]
         
         # Make a packet with the data to filter.
-        n_data = 30
+        n_data = 100
         input_width = 16
         se_input_width = 16
         max_input = pow(2, input_width-1)-1
@@ -59,6 +59,19 @@ class TestNocBlockFirFilter(unittest.TestCase):
                         for i in range(n_data)]
         data = [signal.complex_to_uint(c, width=se_input_width)
                 for c in complex_data]
+        expected_data = []
+        extended_data = [0] * (n_coefficients-1) + complex_data
+        for i in range(n_data):
+            expected = sum([a*b for a, b in zip(
+                coefficients, extended_data[i: i+n_coefficients])])
+            expected /= pow(2, coeff_width)
+            maxval = pow(2, 15)-1
+            minval = -pow(2, 15)
+            def trunc_round(v):
+                return round(max(minval, min(maxval, v)))
+            expected = trunc_round(expected.real) + 1j * trunc_round(expected.imag)
+            expected_data.append(expected)
+                           
         data_packet = chdr.CHDRPacket.create_data_packet(data)
 
         wait_data = []
@@ -83,6 +96,9 @@ class TestNocBlockFirFilter(unittest.TestCase):
 
         input_data = chdr.packets_to_noc_inputs(settings_packets + [data_packet])
 
+        for i in range(200):
+            input_data.append(noc.make_inputs(o_tready=1))
+
         p = project.FileTestBenchProject.create_or_update(
             interface=interface, directory=directory,
             board=config.default_board,
@@ -96,6 +112,16 @@ class TestNocBlockFirFilter(unittest.TestCase):
         errors, output_data = p.run_simulation(
             input_data=wait_data+input_data, runtime=runtime)
         self.assertEqual(len(errors), 0)
+
+        output_packets = chdr.noc_outputs_to_packets(output_data)
+        # Filter out the settings replies.
+        output_lumps = [p.data() for p in output_packets if not p.is_extension_context]
+        self.assertEqual(len(output_lumps), 1)
+        self.assertEqual(len(output_lumps[0]), n_data)
+        output_uints = output_lumps[0]
+        output_cs = [signal.uint_to_complex(ui, width=se_input_width)
+                     for ui in output_uints]
+        self.assertEqual(output_cs, expected_data)
 
         
 if __name__ == '__main__':
